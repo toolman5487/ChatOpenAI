4fb5c63680689b9a41ed1ee3bf7fa3d006d10abe
ToolManHsu <willy548798@gmail.com>
2025-06-11 21:52:33 +0800

    Initial Commit

---
+++ ChatOpenAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatOpenAI
+//
+//  Created by Willy Hsu on 2025/6/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
